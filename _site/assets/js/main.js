246c04d51f02c2466b2ecc146f3dd6e8b262ca9f
KartikP <kartikspradeepan@gmail.com>
2024-01-10 14:51:24 -0500

    logo

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -4,12 +4,12 @@ document.addEventListener(
         new SweetScroll({});
         particlesJS("particles-js", {
             particles: {
-                number: { value: 50, density: { enable: !0, value_area: 500 } },
+                number: { value: 25, density: { enable: !0, value_area: 500 } },
                 color: { value: "#ffffff" },
                 shape: { type: "circle", stroke: { width: 0, color: "#000000" }, polygon: { nb_sides: 5 }, image: { src: "img/github.svg", width: 100, height: 100 } },
                 opacity: { value: 1, random: 1, anim: { enable: 1, speed: 1, opacity_min: 0, sync: 0 } },
                 size: { value: 5, random: !0, anim: { enable: 0, speed: 10, size_min: 0.3, sync: !1 } },
-                line_linked: { enable: 1, distance: 130, color: "#ffffff", opacity: 0.4, width: 1 },
+                line_linked: { enable: 1, distance: 250, color: "#ffffff", opacity: 0.4, width: 1 },
                 move: { enable: !0, speed: 1, direction: "none", random: !0, straight: !1, out_mode: "out", bounce: !1, attract: { enable: !1, rotateX: 600, rotateY: 600 } },
             },
             interactivity: {
